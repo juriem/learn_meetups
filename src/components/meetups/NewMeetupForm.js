37f7add84a869275830767ec2432f4ef17e5c146
Juri Em <juriem1974@gmail.com>
2023-10-31 22:56:13 +0200

    backend logic

--- src/components/meetups/NewMeetupForm.js
+++ src/components/meetups/NewMeetupForm.js
@@ -1,25 +1,46 @@
 import classes from './NewMeetupForm.module.css';
 import Card from "../ui/Card";
+import {useRef} from "react";
 
-function NewMeetupForm() {
+function NewMeetupForm(props) {
+    const titleInputRef = useRef();
+    const imageRef = useRef();
+    const addressRef = useRef();
+    const descriptionRef = useRef();
+
+    function submitHandler(e) {
+        e.preventDefault();
+        const title = titleInputRef.current.value;
+        const image = imageRef.current.value;
+        const address = addressRef.current.value;
+        const description = descriptionRef.current.value;
+        const meetupData = {
+            title,
+            image,
+            address,
+            description
+        };
+        props.onAddMeetup(meetupData);
+    }
+
     return (
         <Card>
-            <form className={classes.form}>
+            <form className={classes.form} onSubmit={submitHandler}>
                 <div className={classes.control}>
                     <label htmlFor="title">Title</label>
-                    <input type="text" id="title" required />
+                    <input type="text" id="title" required ref={titleInputRef}/>
                 </div>
                 <div className={classes.control}>
                     <label htmlFor="image">Image</label>
-                    <input type="url" id="image" required />
+                    <input type="url" id="image" required ref={imageRef} />
                 </div>
                 <div className={classes.control}>
                     <label htmlFor="address">Address</label>
-                    <input type="text" id="address" required />
+                    <input type="text" id="address" required ref={addressRef} />
                 </div>
                 <div className={classes.control}>
                     <label htmlFor="description">Description</label>
-                    <textarea id="description" required />
+                    <textarea id="description" required  ref={descriptionRef}/>
                 </div>
 
                 <div className={classes.actions}>
--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,10 +1,14 @@
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
+    function addMeetupHandler(meetup) {
+
+    }
+
     return (
         <section>
             <h2>New Meetup</h2>
-            <NewMeetupForm />
+            <NewMeetupForm onAddMeetup={addMeetupHandler}/>
         </section>
     )
 }
